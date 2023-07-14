8a1ddb374b63f55d133ad52c6433563620c44fa3
Alan Jimenez Arandia <aljiar23@gmail.com>
2023-07-14 17:00:57 -0400

    Migrate project to Vue 3 and Vuetify 3.

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,9 +2,7 @@ import Vue from 'vue';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
 
-Vue.use(Vuetify);
-
-export default new Vuetify({
+Vue.use(Vuetify, {
   theme: {
       options: {
         customProperties: true,
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,21 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
+// vue.config.js
+module.exports = {
+  chainWebpack: (config) => {
+    config.resolve.alias.set('vue', '@vue/compat')
 
-  
-})
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap((options) => {
+        return {
+          ...options,
+          compilerOptions: {
+            compatConfig: {
+              MODE: 2
+            }
+          }
+        }
+      })
+  }
+}
+
